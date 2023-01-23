2598fb3e85dba6c63cfdfa05fb2ca7d0e1ea9382
IvanKristiawan <101783828+IvanKristiawan@users.noreply.github.com>
2023-01-23 07:45:53 +0700

    Change TPS

---
+++ src/pages/DaftarTps/TambahTps.js
@@ -0,0 +1,426 @@
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from "../../contexts/AuthContext";
+import { tempUrl } from "../../contexts/ContextProvider";
+import { Colors } from "../../constants/styles";
+import { Loader } from "../../components";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Divider,
+  Snackbar,
+  Alert,
+  Paper,
+  Autocomplete
+} from "@mui/material";
+import SaveIcon from "@mui/icons-material/Save";
+
+const TambahTps = () => {
+  const { user } = useContext(AuthContext);
+  const [open, setOpen] = useState(false);
+  const [caleg, setCaleg] = useState("");
+  const [kecamatan, setKecamatan] = useState("");
+  const [noTps, setNoTps] = useState("");
+  const [namaTps, setNamaTps] = useState("");
+  const [noHpSaksi, setNoHpSaksi] = useState("");
+  const [namaSaksi, setNamaSaksi] = useState("");
+  const [targetSuara, setTargetSuara] = useState("");
+  const [passwordSaksi, setPasswordSaksi] = useState("");
+  const [error, setError] = useState(false);
+  const [calegs, setCalegs] = useState([]);
+  const [kecamatans, setKecamatans] = useState([]);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const calegOptions = calegs.map((caleg) => ({
+    label: `${caleg._id} - ${caleg.nama}`
+  }));
+
+  const kecamatanOptions = kecamatans.map((kecamatan) => ({
+    label: `${kecamatan._id} - ${kecamatan.namaKecamatan}`
+  }));
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    getCalegsData();
+    if (user.tipeUser === "ADMIN") {
+      getKecamatansData();
+    } else {
+      getKecamatansCalegData();
+    }
+  }, []);
+
+  const getCalegsData = async () => {
+    setLoading(true);
+    const allCalegs = await axios.post(`${tempUrl}/usersCaleg`, {
+      id: user._id,
+      token: user.token
+    });
+    setCalegs(allCalegs.data);
+    setLoading(false);
+  };
+
+  const getKecamatansData = async () => {
+    setLoading(true);
+    const kecamatans = await axios.post(`${tempUrl}/kecamatans`, {
+      id: user._id,
+      token: user.token
+    });
+    setKecamatans(kecamatans.data);
+    setLoading(false);
+  };
+
+  const getKecamatansCalegData = async () => {
+    setLoading(true);
+    const kecamatans = await axios.post(`${tempUrl}/kecamatansCaleg`, {
+      id: user._id,
+      token: user.token
+    });
+    setKecamatans(kecamatans.data);
+    setLoading(false);
+  };
+
+  const getNextKodeTpsCaleg = async (value) => {
+    const nextKodeTps = await axios.post(`${tempUrl}/tpsNextKode`, {
+      idCaleg: user._id,
+      id: user._id,
+      token: user.token
+    });
+    setKecamatan(value);
+    if (user.tipeUser === "ADMIN") {
+      setNoTps(`${value}`);
+    } else {
+      setNoTps(`${value}${nextKodeTps.data}`);
+    }
+  };
+
+  const getNextKodeTpsAdmin = async (value) => {
+    const findCaleg = await axios.post(`${tempUrl}/findUser/${value}`, {
+      id: user._id,
+      token: user.token
+    });
+    if (findCaleg.data) {
+      const nextKodeTps = await axios.post(`${tempUrl}/tpsNextKode`, {
+        idCaleg: findCaleg.data._id,
+        id: user._id,
+        token: user.token
+      });
+      setNoTps(`${kecamatan}${nextKodeTps.data}`);
+    }
+    setCaleg(value);
+  };
+
+  const saveTps = async (e) => {
+    let calegId = user._id;
+    e.preventDefault();
+    var date = new Date();
+    var current_date =
+      date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+    var current_time =
+      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    let isFailedValidation =
+      noTps.length === 0 ||
+      kecamatan.length === 0 ||
+      namaTps.length === 0 ||
+      noHpSaksi.length === 0 ||
+      namaSaksi.length === 0 ||
+      targetSuara.length === 0 ||
+      passwordSaksi.length === 0;
+    if (isFailedValidation) {
+      setError(true);
+      setOpen(!open);
+    } else {
+      try {
+        setLoading(true);
+        if (user.tipeUser === "ADMIN") {
+          const findCaleg = await axios.post(`${tempUrl}/findUser/${caleg}`, {
+            id: user._id,
+            token: user.token
+          });
+          calegId = findCaleg.data._id;
+        }
+        await axios.post(`${tempUrl}/saveTps`, {
+          idCaleg: calegId,
+          idKecamatan: kecamatan,
+          noTps,
+          namaTps,
+          noHpSaksi,
+          namaSaksi,
+          targetSuara,
+          passwordSaksi,
+          tglInput: current_date,
+          jamInput: current_time,
+          userInput: user.nama,
+          id: user._id,
+          token: user.token
+        });
+        setLoading(false);
+        navigate("/daftarTps");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <Box sx={container}>
+      <Typography color="#757575">Daftar TPS</Typography>
+      <Typography variant="h4" sx={subTitleText}>
+        Tambah TPS
+      </Typography>
+      <Divider sx={dividerStyle} />
+      <Paper sx={contentContainer} elevation={12}>
+        <Box sx={showDataContainer}>
+          <Box sx={showDataWrapper}>
+            <Typography sx={labelInput}>No. TPS</Typography>
+            <TextField
+              size="small"
+              error={error && noTps.length === 0 && true}
+              helperText={error && noTps.length === 0 && "No. TPS harus diisi!"}
+              id="outlined-basic"
+              variant="outlined"
+              value={noTps}
+              InputProps={{
+                readOnly: true
+              }}
+              sx={{ backgroundColor: Colors.grey400 }}
+            />
+            <Typography sx={[labelInput, spacingTop]}>
+              Kode Kecamatan
+            </Typography>
+            <Autocomplete
+              size="small"
+              disablePortal
+              id="combo-box-demo"
+              options={kecamatanOptions}
+              renderInput={(params) => (
+                <TextField
+                  size="small"
+                  error={error && kecamatan.length === 0 && true}
+                  helperText={
+                    error &&
+                    kecamatan.length === 0 &&
+                    "Kode Kecamatan harus diisi!"
+                  }
+                  {...params}
+                />
+              )}
+              onInputChange={(e, value) => {
+                if (value) {
+                  getNextKodeTpsCaleg(value.split(" ", 1)[0]);
+                } else {
+                  setNoTps("");
+                }
+              }}
+            />
+            {user.tipeUser === "ADMIN" && (
+              <>
+                <Typography sx={[labelInput, spacingTop]}>
+                  Kode Caleg
+                </Typography>
+                <Autocomplete
+                  size="small"
+                  disablePortal
+                  id="combo-box-demo"
+                  options={calegOptions}
+                  renderInput={(params) => (
+                    <TextField
+                      size="small"
+                      error={error && caleg.length === 0 && true}
+                      helperText={
+                        error && caleg.length === 0 && "Caleg harus diisi!"
+                      }
+                      {...params}
+                    />
+                  )}
+                  onInputChange={(e, value) => {
+                    if (value) {
+                      getNextKodeTpsAdmin(value.split(" ", 1)[0]);
+                    } else {
+                      setNoTps("");
+                    }
+                  }}
+                />
+              </>
+            )}
+            <Typography sx={[labelInput, spacingTop]}>Nama TPS</Typography>
+            <TextField
+              size="small"
+              error={error && namaTps.length === 0 && true}
+              helperText={
+                error && namaTps.length === 0 && "Nama TPS harus diisi!"
+              }
+              id="outlined-basic"
+              variant="outlined"
+              value={namaTps}
+              onChange={(e) => setNamaTps(e.target.value.toUpperCase())}
+            />
+          </Box>
+          <Box sx={[showDataWrapper, secondWrapper]}>
+            <Typography sx={labelInput}>Nama Saksi</Typography>
+            <TextField
+              size="small"
+              error={error && namaSaksi.length === 0 && true}
+              helperText={
+                error && namaSaksi.length === 0 && "Nama Saksi harus diisi!"
+              }
+              id="outlined-basic"
+              variant="outlined"
+              value={namaSaksi}
+              onChange={(e) => setNamaSaksi(e.target.value.toUpperCase())}
+            />
+            <Typography sx={[labelInput, spacingTop]}>No. HP Saksi</Typography>
+            <TextField
+              type="number"
+              error={error && noHpSaksi.length === 0 && true}
+              helperText={
+                error && noHpSaksi.length === 0 && "No. HP Saksi harus diisi!"
+              }
+              size="small"
+              id="outlined-basic"
+              variant="outlined"
+              value={noHpSaksi}
+              onChange={(e) => setNoHpSaksi(e.target.value.toUpperCase())}
+            />
+            <Typography sx={[labelInput, spacingTop]}>
+              Target Suara
+              {targetSuara !== 0 &&
+                !isNaN(parseInt(targetSuara)) &&
+                ` : ${parseInt(targetSuara).toLocaleString()}`}
+            </Typography>
+            <TextField
+              type="number"
+              error={error && targetSuara.length === 0 && true}
+              helperText={
+                error && targetSuara.length === 0 && "Target Suara harus diisi!"
+              }
+              size="small"
+              id="outlined-basic"
+              variant="outlined"
+              value={targetSuara}
+              onChange={(e) => setTargetSuara(e.target.value.toUpperCase())}
+            />
+            <Typography sx={[labelInput, spacingTop]}>
+              Password Saksi
+            </Typography>
+            <TextField
+              type="password"
+              size="small"
+              error={error && passwordSaksi.length === 0 && true}
+              helperText={
+                error &&
+                passwordSaksi.length === 0 &&
+                "Password Saksi Dealer harus diisi!"
+              }
+              id="outlined-basic"
+              variant="outlined"
+              value={passwordSaksi}
+              onChange={(e) => setPasswordSaksi(e.target.value.toUpperCase())}
+            />
+          </Box>
+        </Box>
+        <Box sx={spacingTop}>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={() => navigate("/daftarTps")}
+            sx={{ marginRight: 2 }}
+          >
+            {"< Kembali"}
+          </Button>
+          <Button
+            variant="contained"
+            startIcon={<SaveIcon />}
+            onClick={saveTps}
+          >
+            Simpan
+          </Button>
+        </Box>
+      </Paper>
+      <Divider sx={spacingTop} />
+      {error && (
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error" sx={alertBox}>
+            Data belum terisi semua!
+          </Alert>
+        </Snackbar>
+      )}
+    </Box>
+  );
+};
+
+export default TambahTps;
+
+const container = {
+  p: 4
+};
+
+const subTitleText = {
+  fontWeight: "900"
+};
+
+const dividerStyle = {
+  mt: 2
+};
+
+const showDataContainer = {
+  mt: 4,
+  display: "flex",
+  flexDirection: {
+    xs: "column",
+    sm: "row"
+  }
+};
+
+const showDataWrapper = {
+  display: "flex",
+  flex: 1,
+  flexDirection: "column",
+  maxWidth: {
+    md: "40vw"
+  }
+};
+
+const spacingTop = {
+  mt: 4
+};
+
+const alertBox = {
+  width: "100%"
+};
+
+const labelInput = {
+  fontWeight: "600",
+  marginLeft: 1
+};
+
+const contentContainer = {
+  p: 3,
+  pt: 1,
+  mt: 2,
+  backgroundColor: Colors.grey100
+};
+
+const secondWrapper = {
+  marginLeft: {
+    sm: 4
+  },
+  marginTop: {
+    sm: 0,
+    xs: 4
+  }
+};
---
+++ src/pages/DaftarTps/UbahTps.js
@@ -0,0 +1,332 @@
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from "../../contexts/AuthContext";
+import { tempUrl } from "../../contexts/ContextProvider";
+import { Colors } from "../../constants/styles";
+import { Loader } from "../../components";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Divider,
+  Snackbar,
+  Alert,
+  Paper
+} from "@mui/material";
+import SaveIcon from "@mui/icons-material/Save";
+
+const UbahTps = () => {
+  const { user } = useContext(AuthContext);
+  const [open, setOpen] = useState(false);
+  const [caleg, setCaleg] = useState("");
+  const [kecamatan, setKecamatan] = useState("");
+  const [noTps, setNoTps] = useState("");
+  const [namaTps, setNamaTps] = useState("");
+  const [noHpSaksi, setNoHpSaksi] = useState("");
+  const [namaSaksi, setNamaSaksi] = useState("");
+  const [jumlahPemilih, setJumlahPemilih] = useState("");
+  const [targetSuara, setTargetSuara] = useState("");
+  const [passwordSaksi, setPasswordSaksi] = useState("");
+  const [passwordSaksiAwal, setPasswordSaksiAwal] = useState("");
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    getTpsById();
+  }, []);
+
+  const getTpsById = async () => {
+    setLoading(true);
+    const pickedTps = await axios.post(`${tempUrl}/tps/${id}`, {
+      id: user._id,
+      token: user.token
+    });
+    setNoTps(pickedTps.data.noTps);
+    setNamaTps(pickedTps.data.namaTps);
+    setNoHpSaksi(pickedTps.data.noHpSaksi);
+    setNamaSaksi(pickedTps.data.namaSaksi);
+    setJumlahPemilih(pickedTps.data.jumlahPemilih);
+    setTargetSuara(pickedTps.data.targetSuara);
+    setPasswordSaksiAwal(pickedTps.data.passwordSaksi);
+    setCaleg(pickedTps.data.idCaleg.nama);
+    setKecamatan(
+      `${pickedTps.data.idKecamatan._id} - ${pickedTps.data.idKecamatan.namaKecamatan}`
+    );
+    setLoading(false);
+  };
+
+  const updateTps = async (e) => {
+    e.preventDefault();
+    var date = new Date();
+    var current_date =
+      date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+    var current_time =
+      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    let isFailedValidation =
+      namaTps.length === 0 ||
+      noHpSaksi.length === 0 ||
+      namaSaksi.length === 0 ||
+      targetSuara.length === 0;
+    if (isFailedValidation) {
+      setError(true);
+      setOpen(!open);
+    } else {
+      try {
+        setLoading(true);
+        await axios.post(`${tempUrl}/updateTps/${id}`, {
+          namaTps,
+          noHpSaksi,
+          namaSaksi,
+          targetSuara,
+          passwordSaksi: !passwordSaksi ? passwordSaksiAwal : passwordSaksi,
+          tglUpdate: current_date,
+          jamUpdate: current_time,
+          userUpdate: user.nama,
+          id: user._id,
+          token: user.token
+        });
+        setLoading(false);
+        navigate("/daftarTps");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <Box sx={container}>
+      <Typography color="#757575">Daftar TPS</Typography>
+      <Typography variant="h4" sx={subTitleText}>
+        Tambah TPS
+      </Typography>
+      <Divider sx={dividerStyle} />
+      <Paper sx={contentContainer} elevation={12}>
+        <Box sx={showDataContainer}>
+          <Box sx={showDataWrapper}>
+            <Typography sx={labelInput}>Caleg</Typography>
+            <TextField
+              size="small"
+              id="outlined-basic"
+              variant="outlined"
+              value={caleg}
+              InputProps={{
+                readOnly: true
+              }}
+              sx={{ backgroundColor: Colors.grey400 }}
+            />
+            <Typography sx={[labelInput, spacingTop]}>Kecamatan</Typography>
+            <TextField
+              size="small"
+              id="outlined-basic"
+              variant="outlined"
+              value={kecamatan}
+              InputProps={{
+                readOnly: true
+              }}
+              sx={{ backgroundColor: Colors.grey400 }}
+            />
+            <Typography sx={[labelInput, spacingTop]}>No. TPS</Typography>
+            <TextField
+              size="small"
+              id="outlined-basic"
+              variant="outlined"
+              value={noTps}
+              InputProps={{
+                readOnly: true
+              }}
+              sx={{ backgroundColor: Colors.grey400 }}
+            />
+            <Typography sx={[labelInput, spacingTop]}>Nama TPS</Typography>
+            <TextField
+              size="small"
+              error={error && namaTps.length === 0 && true}
+              helperText={
+                error && namaTps.length === 0 && "Nama TPS harus diisi!"
+              }
+              id="outlined-basic"
+              variant="outlined"
+              value={namaTps}
+              onChange={(e) => setNamaTps(e.target.value.toUpperCase())}
+            />
+          </Box>
+          <Box sx={[showDataWrapper, secondWrapper]}>
+            <Typography sx={labelInput}>Nama Saksi</Typography>
+            <TextField
+              size="small"
+              error={error && namaSaksi.length === 0 && true}
+              helperText={
+                error && namaSaksi.length === 0 && "Nama Saksi harus diisi!"
+              }
+              id="outlined-basic"
+              variant="outlined"
+              value={namaSaksi}
+              onChange={(e) => setNamaSaksi(e.target.value.toUpperCase())}
+            />
+            <Typography sx={[labelInput, spacingTop]}>No. HP Saksi</Typography>
+            <TextField
+              type="number"
+              error={error && noHpSaksi.length === 0 && true}
+              helperText={
+                error && noHpSaksi.length === 0 && "No. HP Saksi harus diisi!"
+              }
+              size="small"
+              id="outlined-basic"
+              variant="outlined"
+              value={noHpSaksi}
+              onChange={(e) => setNoHpSaksi(e.target.value.toUpperCase())}
+            />
+            <Typography sx={[labelInput, spacingTop]}>
+              Jumlah Pemilih
+            </Typography>
+            <TextField
+              tpye="number"
+              size="small"
+              id="outlined-basic"
+              variant="outlined"
+              value={jumlahPemilih}
+              InputProps={{
+                readOnly: true
+              }}
+              sx={{ backgroundColor: Colors.grey400 }}
+            />
+            <Typography sx={[labelInput, spacingTop]}>
+              Target Suara
+              {targetSuara !== 0 &&
+                !isNaN(parseInt(targetSuara)) &&
+                ` : ${parseInt(targetSuara).toLocaleString()}`}
+            </Typography>
+            <TextField
+              tpye="number"
+              error={error && targetSuara.length === 0 && true}
+              helperText={
+                error && targetSuara.length === 0 && "Target Suara harus diisi!"
+              }
+              size="small"
+              id="outlined-basic"
+              variant="outlined"
+              value={targetSuara}
+              onChange={(e) => setTargetSuara(e.target.value.toUpperCase())}
+            />
+            <Typography sx={[labelInput, spacingTop]}>
+              Password Saksi
+            </Typography>
+            <TextField
+              type="password"
+              size="small"
+              id="outlined-basic"
+              variant="outlined"
+              value={passwordSaksi}
+              onChange={(e) => setPasswordSaksi(e.target.value.toUpperCase())}
+            />
+            <Typography>
+              *Kosongkan jika tidak ingin mengganti password
+            </Typography>
+          </Box>
+        </Box>
+        <Box sx={spacingTop}>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={() => navigate("/daftarTps")}
+            sx={{ marginRight: 2 }}
+          >
+            {"< Kembali"}
+          </Button>
+          <Button
+            variant="contained"
+            startIcon={<SaveIcon />}
+            onClick={updateTps}
+          >
+            Ubah
+          </Button>
+        </Box>
+      </Paper>
+      <Divider sx={spacingTop} />
+      {error && (
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error" sx={alertBox}>
+            Data belum terisi semua!
+          </Alert>
+        </Snackbar>
+      )}
+    </Box>
+  );
+};
+
+export default UbahTps;
+
+const container = {
+  p: 4
+};
+
+const subTitleText = {
+  fontWeight: "900"
+};
+
+const dividerStyle = {
+  mt: 2
+};
+
+const showDataContainer = {
+  mt: 4,
+  display: "flex",
+  flexDirection: {
+    xs: "column",
+    sm: "row"
+  }
+};
+
+const showDataWrapper = {
+  display: "flex",
+  flex: 1,
+  flexDirection: "column",
+  maxWidth: {
+    md: "40vw"
+  }
+};
+
+const spacingTop = {
+  mt: 4
+};
+
+const alertBox = {
+  width: "100%"
+};
+
+const labelInput = {
+  fontWeight: "600",
+  marginLeft: 1
+};
+
+const contentContainer = {
+  p: 3,
+  pt: 1,
+  mt: 2,
+  backgroundColor: Colors.grey100
+};
+
+const secondWrapper = {
+  marginLeft: {
+    sm: 4
+  },
+  marginTop: {
+    sm: 0,
+    xs: 4
+  }
+};
